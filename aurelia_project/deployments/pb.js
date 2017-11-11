679b02cc102fa90f6de237471ead24bf59caacd8
Vysakh Sreenivasan <diplomatv@gmail.com>
2017-11-11 12:35:58 +0530

    Basic setup for malaysia

--- aurelia_project/deployments/pb.js
+++ aurelia_project/deployments/pb.js
@@ -1,40 +1,27 @@
 export default {
-  name: 'petabencana',
+  name: 'riskmap_my',
   height_units: 'cm',
   map: {
     'instance_regions': {
-      'jakarta': {
-        'region': 'jbd',
-        'bounds': {
-          'sw': [-6.733, 106.480],
-          'ne': [-5.880, 107.175]
-        }
-      },
-      'surabaya': {
-        'region': 'sby',
-        'bounds': {
-          'sw': [-7.5499, 112.3975],
-          'ne': [-7.0143, 113.0318]
-        }
-      },
-      'bandung': {
-        'region': 'bdg',
-        'bounds': {
-          'sw': [-7.165, 107.369],
-          'ne': [-6.668, 107.931]
+      'penang': {
+        'region': 'png',
+        'bounds': { //arbit bounding box drawn in QGIS. So the edges aren't orthogonal
+          'sw': [5.122673, 100.1023307000],
+          'ne': [5.5974837, 100.550619999]
         }
       }
     },
     'default_region': {
-      'region': 'java',
+      'region': 'chn',
       'bounds': {
-        'sw': [-10.293, 104.677],
-        'ne': [-3.974, 115.290]
+        'sw': [12.6884, 79.9248],
+        'ne': [13.3766, 80.5413]
       }
     },
-    'region_center': [-7, 110],
-    'start_city_center': [-6.1754, 106.8271],
-    'starting_zoom': 8,
-    'minimum_zoom': 8
+    'region_center': [5.4163459, 100.33276169],
+    'start_city_center': [5.4163459, 100.33276169],
+    'starting_zoom': 10,
+    'minimum_zoom': 10
   }
 };
+
--- aurelia_project/deployments/rm.js
+++ aurelia_project/deployments/rm.js
@@ -24,3 +24,4 @@ export default {
     'minimum_zoom': 10
   }
 };
+
